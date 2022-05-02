31e026b62ccb5e1d6b536e14f571d8c91f47e448
Jimi Lee <46700478+jimilee@users.noreply.github.com>
2022-05-02 16:39:18 +0900

    Update make_csv.py

--- docker_example/make_csv.py
+++ docker_example/make_csv.py
@@ -1,16 +1,31 @@
 import os
 import glob
 import pandas as pd
-def make_csv_file(path):
-  """
+"""
   path : image folder path
   result : dict { PATH : image folder full path name , labels : image label
-  """
-  labels = []
-  img_path = []
-  for label in os.listdir(path):
-    img_path.extend(glob.glob(os.path.join(path,label)+ '/*.jpg'))
-    labels.extend([label]*len(glob.glob(os.path.join(path,label)+ '/*.jpg')))
-  csv = pd.DataFrame({'PATH' : img_path,
+"""
+def make_csv_file(path):
+    labels = []
+    img_path = []
+    for label in os.listdir(path):
+        img_path.extend(glob.glob(os.path.join(path,label)+ '/*.jpg'))
+        labels.extend([label]*len(glob.glob(os.path.join(path,label)+ '/*.jpg')))
+    csv = pd.DataFrame({'PATH' : img_path,
                       'labels' : labels})
-  return csv
+    return csv
+
+###############   make csv in aws ################
+def aws_make_csv_file(path, conn, bucket, subfolder):
+    contents = conn.list_objects(Bucket=bucket, Prefix=subfolder+path)['Contents']
+
+    labels = []
+    img_path = []
+    for label in contents:
+        img_path.extend([f's3://'+bucket+'/'+label['Key']])
+        labels.extend([label['Key'][len(subfolder+path)+1:].split('/')[0]])
+    print(img_path[0])
+    print(labels[0])
+    csv = pd.DataFrame({'PATH' : img_path, 'labels' : labels})
+        
+    return csv
